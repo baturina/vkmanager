782533ea51ad5e0e6a0744121a60f6d5686ee16f
Anastasia Baturina <89099934219@mail.ru>
2021-01-22 00:46:37 +0300

    v2

---
+++ src/main/java/domain/MeterView.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class MeterView {
+
+    private int meter;
+}
+
---
+++ src/test/java/domain/PostManager.java
@@ -0,0 +1,15 @@
+package domain;
+public class PostManager {
+
+
+
+        private Post[] posts;
+
+        public Post search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+            return null;
+        }
+
+        public Post delete(int ownerId, int postId) {
+            return null;
+        }
+    }
